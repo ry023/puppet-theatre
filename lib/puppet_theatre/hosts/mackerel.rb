d23dc30363bec9a487ebcf10868bdb439992a116
Kasumi Hanazuki <kasumi@rollingapple.net>
2016-03-14 16:19:17 +0900

    Fix mackerel host generator

--- lib/puppet_theatre/hosts/mackerel.rb
+++ lib/puppet_theatre/hosts/mackerel.rb
@@ -4,13 +4,18 @@ module PuppetTheatre
   module Hosts
     class Mackerel < Base
       def each
+        client.hosts(service: config.fetch(:service)).each do |host|
+          yield host.name
+        end
+      end
+
+      private
+
+      def client
         @client ||= ::Mackerel::Client.new.tap do |client|
           client.configure do |c|
             c.api_key = config.fetch(:api_key)
           end
-        end
-        @client.hosts(service: config.fetch(:service)).each do |host|
-          yield name
         end
       end
     end
