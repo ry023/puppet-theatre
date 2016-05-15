3d8ce2bbb905fc074b67759ae89a853e06939cdb
Kasumi Hanazuki <kasumi@rollingapple.net>
2016-05-15 21:48:32 +0900

    Update findable.rb

--- lib/puppet_theatre/findable.rb
+++ lib/puppet_theatre/findable.rb
@@ -3,10 +3,10 @@ module PuppetTheatre
     def find_class(name, mod, path)
       klsname = name.to_s.gsub(/(?:\A|_)./) {|s| s[-1].upcase }.intern
       begin
-        return mod.const_get(klsname)
+        return mod.const_get(klsname, false)
       rescue NameError
         require [path, name].join(?/)
-        return mod.const_get(klsname)
+        return mod.const_get(klsname, false)
       end
     end
   end
